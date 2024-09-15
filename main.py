27ec2c2733acc605d465c9b133b6aca8bea9a53f
Anssi Laukkarinen <anssi.laukkarinen@gmail.com>
2024-09-15 11:18:41 +0300

    Small editing of code layout

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ Created on Sat Sep 14 22:16:51 2024
 
 @author: Anssi Laukkarinen
 
+The purpose of this code is to read in hourly climate data from csv
+files and export it to epw climate files.
+
+
+Input climate files are from the Finnish Meteorological Institute.
+https://www.ilmatieteenlaitos.fi/rakennusten-energialaskennan-testivuosi
 
 Input file column names:
 STEP;YEAR;MON;DAY;HOUR;TEMP;RH;WS;WDIR;GHI;DHI;DNI
+
+
+
+Information on the epw file format:
+https://bigladdersoftware.com/epx/docs/23-2/auxiliary-programs/energyplus-weather-file-epw-data-dictionary.html#energyplus-weather-file-epw-data-dictionary
 
 
 
@@ -18,8 +29,10 @@ import pandas as pd
 
 import pvlib
 
+import helper
 
-root_folder = r'C:\Users\laukkara\github\txt_to_epw'
+
+root_folder = r'C:\Users\laukkara\github\csv_to_epw'
 
 input_folder = os.path.join(root_folder,
                             'input')
@@ -29,297 +42,6 @@ output_folder = os.path.join(root_folder,
 
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
-
-
-
-
-###################
-
-def get_epw_timestamps(df_):
-    
-    # The input data has hours 0-23
-    # We have to adjust timestamps where hour is 0
-    # Other timestamps can be kept as they are
-    
-    # Both are already defined in such a way that the timestamps represents
-    # the previous hour
-    
-    year_output = 2023
-    
-    timestamps = []
-    
-    for idx, row in df_.iterrows():
-        
-        year = 999
-        month = 999
-        day = 999
-        hour = 999
-        
-        if idx.hour == 0:
-            # First hour of the day
-            
-            
-            if idx.day == 1:
-                # First day of month
-                # The length of the previous month varies
-                
-                if idx.month in [1]:
-                    # First month of the year
-                    # Year changes also
-                    
-                    # TODO: The current change of year doesn't work universally
-                    # It assumes that there is no timestamp for the yyyy-01-01T00:00,
-                    # but the last row is duplicate of the second-last row.
-                    hour = 24
-                    day = 31
-                    month = 12
-                    year = year_output
-                    
-                elif idx.month in [3]:
-                    # 0-23 system: 1.3.2023 00:00:00 == 1-24 system: 28.2.2023 24:00:00,
-                    # when there is no leap days. Year 2023 doesn't have leap days.
-                    hour = 24
-                    day = 28
-                    month = idx.month - 1
-                    year = year_output
-                
-                # Other months of the year
-                elif idx.month in [5, 7, 10, 12]:
-                    hour = 24
-                    day = 30
-                    month = idx.month - 1
-                    year = year_output
-                    
-                elif idx.month in [2, 4, 6, 8, 9, 11]:
-                    hour = 24
-                    day = 31
-                    month = idx.month - 1
-                    year = year_output
-                    
-                else:
-                    print('unknown month, error')
-                
-            else:
-                # Other days of the month
-                hour = 24
-                day = idx.day - 1
-                month = idx.month
-                year = year_output
-                
-            
-            
-            
-        else:
-            # Other hours of the day
-            
-            year = year_output
-            month = idx.month
-            day = idx.day
-            hour = idx.hour
-        
-        timestamp = [year, month, day, hour, 0, 0]
-        timestamps.append(timestamp)
-    
-    
-    timestamps_np = np.array(timestamps)
-    
-    return(timestamps_np)
-
-
-
-
-def calc_pvsat_water(Tdb_):
-    
-    # Saturation vapor pressure is calculated alwasy with regards 
-    # to liquid water
-    
-    pvsat = 610.5 * np.exp((17.269*Tdb_)/(237.3+Tdb_))
-    
-    return(pvsat)
-
-
-
-
-def calc_Tdp(Tdb_, RHwater_):
-    
-    # [Tdp] = degC
-    
-    pv = (RHwater_/100.0) * calc_pvsat_water(Tdb_)
-    
-    Tdp_ = []
-    
-    pvval_previous = 500.0
-    
-    for pvval in pv:
-        # print(pvval, flush=True)
-        
-        if pvval < 10.0:
-            pvval = pvval_previous
-            pvval_previous = 500.0
-        else:
-            pvval_previous = pvval
-        
-        if pvval >= 610.5:
-            
-            theta = (237.3*np.log(pvval/610.5)) / (17.269-np.log(pvval/610.5))
-        
-        else:
-            
-            theta = (265.5*np.log(pvval/610.5)) / (21.875-np.log(pvval/610.5))
-        
-        
-        Tdp_.append(theta)
-    
-    Tdp_ = np.array(Tdp_)    
-    
-    return(Tdp_)
-
-
-
-
-
-def calc_LWdn(input_dict, method='dTsky'):
-    
-    
-    sigma_SB = 5.67e-8    
-    
-    if method == 'dTsky':
-        
-        Tdb_ = input_dict['Tdb']
-        
-        dT_sky = 11.0
-        Tsky_K = (Tdb_+273.15) - dT_sky
-        LWdn = sigma_SB * Tsky_K**4
-        
-        return(LWdn)
-    
-    elif method == 'clearness':
-        
-        T2m_K = input_dict['Tdb'] + 273.15
-        phi_ = input_dict['RH'] / 100.0
-        
-        pvsat_ = calc_pvsat_water(input_dict['Tdb'])
-        e_a_in_hPa = phi_ * pvsat_ / 100.0
-        
-        ghi_extra_ = input_dict['ghi_extra']    
-        ghi_ = input_dict['ghi']
-        
-        # Clearness index Kt, 0...1
-        Kt = ghi_ / ghi_extra_
-        idxs = ghi_ < 20.0
-        Kt.iloc[idxs] = np.nan
-        Kt.interpolate(inplace=True)
-        
-        idxs_nans = Kt.isna()
-        
-        if np.sum(idxs_nans) > 0:
-            print('  Kt n_nans:', np.sum(idxs_nans), flush=True)
-            Kt.iloc[idxs_nans] = 0.5
-        
-        
-        # Cloudiness c, 0...1
-        # x = Kt.values
-        # xp = (0.25, 0.8)
-        # fp = (1.0, 0.0)
-        # c = np.interp(x, xp, fp)
-        
-        # fig, ax = plt.subplots()
-        # ax.plot(xp, fp, label='Flertchinger')
-        # ax.plot((0,1), (1, 0), label='linear')
-        # ax.legend()
-        
-        # cloud fraction f_c
-        f_c = 1.0 - Kt.values
-        f_c = np.maximum(f_c, 0.0)
-        f_c = np.minimum(f_c, 1.0)
-            
-        
-        # emissivitites
-        emiss_clear_sky = 1.24 * (e_a_in_hPa/T2m_K)**(1/7)
-        emiss_cloudy_sky = 1.0
-        
-        emiss_all_sky = f_c * emiss_cloudy_sky + (1-f_c) * emiss_clear_sky
-        
-        LWdn = emiss_all_sky * sigma_SB * T2m_K**4
-        
-        return(LWdn)
-    
-        
-
-
-
-
-def calc_N_from_LWdn(LWdn_, Tdb_, Tdp_):
-    
-    sigma_SB = 5.67e-8
-    
-    # If LWdn is missing, it is calculated from opaque sky cover
-    # aN**3 + bN**2 + cN + d = 0 -> to find N, solve roots of polynomial
-    x = 0.787 + 0.764*np.log((Tdp_+273.15)/273.15)
-    
-    ds = 1 - LWdn_ / (x * sigma_SB * (Tdb_+273.15)**4)
-    
-    c_coef = 0.0224
-    b_coef = 0.0035
-    a_coef = 0.00028
-    
-    N_list = []
-    
-    for d_coef in ds:
-        
-        coefs = [a_coef, b_coef, c_coef, d_coef]
-        
-        roots_from_np = np.roots(coefs)
-        idxs_real = np.isreal(roots_from_np)
-        
-        if idxs_real.sum() == 0:
-            # no solution
-            
-            N = 5.0
-            
-        elif idxs_real.sum() == 1:
-            # one real solution
-            
-            N = np.real(roots_from_np[idxs_real])
-            
-        elif idxs_real.sum() > 1:
-            # more than one real solution
-            
-            Ns = np.real(roots_from_np[idxs_real])
-            
-            idxs_inRange = (Ns >= 0.0) & (Ns <= 10.0)
-            
-            if idxs_inRange.sum() == 0:
-                # No solutions in range
-                
-                N = 5.0
-                
-            elif idxs_inRange.sum() == 1:
-                # One solution in range
-                
-                N = Ns[idxs_inRange]
-                
-            elif idxs_inRange.sum() > 1:
-                # More than one solution in range
-                # Use the average
-                Ns_inRange = Ns[idxs_inRange]
-                N = np.mean(Ns_inRange)
-        
-        N_list.append(N)
-    
-    N_np = np.array(N_list).flatten()
-    
-    N_np = np.maximum(N_np, 0.0)
-    N_np = np.minimum(N_np, 10.0)
-    
-    return(N_np)
-
-
-
-
-
-
-
 
 
 
@@ -377,7 +99,7 @@ for file in os.listdir(input_folder):
     
     df.index = timestamps_pd
     
-    timestamps_np = get_epw_timestamps(df)
+    timestamps_np = helper.get_epw_timestamps(df)
     
     n_hours = df.shape[0]
     
@@ -421,7 +143,7 @@ for file in os.listdir(input_folder):
                      'TYPICAL/EXTREME PERIODS,0',
                      'GROUND TEMPERATURES,0',
                      'HOLIDAYS/DAYLIGHT SAVINGS,No,0,0,0',
-                    f'COMMENTS 1,{file}',
+                    f'COMMENTS 1,"{file}"',
                      'COMMENTS 2,"Finnish Test Reference Year by Finnish Meteorological Institute"',
                      'DATA PERIODS,1,1,Data,Monday,1/1,12/31']
     
@@ -456,8 +178,8 @@ for file in os.listdir(input_folder):
     # 4 - meteorological
     d['N6_Tdb'] = df.loc[:,'TEMP'].values
     
-    d['N7_Tdp'] = calc_Tdp(df.loc[:,'TEMP'].values, 
-                           df.loc[:,'RH'].values)
+    d['N7_Tdp'] = helper.calc_Tdp(df.loc[:,'TEMP'].values, 
+                                  df.loc[:,'RH'].values)
     
     d['N8_RH'] = df.loc[:,'RH'].values
     
@@ -467,7 +189,7 @@ for file in os.listdir(input_folder):
     ## 2 - extraterrestrial radiation for calculating clearness index
     # Calculate extraterrestrial beam radiation
     dni_extra = pvlib.irradiance.get_extra_radiation( \
-                    datetime_or_doy=df.index.shift(freq="-30T"))
+                    datetime_or_doy=df.index.shift(freq="-30min"))
     
     
     
@@ -527,8 +249,8 @@ for file in os.listdir(input_folder):
         input_dict['ghi_extra'] = ghi_extra
         input_dict['ghi'] = ghi
     
-    LWdn = calc_LWdn(input_dict, 
-                     method=LWdn_method)
+    LWdn = helper.calc_LWdn(input_dict, 
+                            method=LWdn_method)
     
     d['N12_HorizontalInfraredRadiationIntensity'] = LWdn
     
@@ -566,7 +288,7 @@ for file in os.listdir(input_folder):
     
     # second code version
     
-    N_np = calc_N_from_LWdn(LWdn, d['N6_Tdb'], d['N7_Tdp'])
+    N_np = helper.calc_N_from_LWdn(LWdn, d['N6_Tdb'], d['N7_Tdp'])
         
     # Energy Plus should use OpaqueSkyCover if LWdn is not available
     # However, IDA-ICE seems to read cloudines from TotalSkyCover column
@@ -617,7 +339,7 @@ for file in os.listdir(input_folder):
     ## Write everything to a text file
     
     fname = os.path.join(output_folder,
-                         file.replace('csv','epw'))
+                         'epw_' + file.replace('csv','epw'))
     
     # Header
     with open(fname, 'w') as f:
